9a3f913231f6ada52ded39ad2dad5ab70c2c3a42
Alex Kalmy <kalmykovalexey01@gmail.com>
2024-07-01 14:35:35 +0500

    Repaired configuration files

--- taxi/data/get_random_address.py
+++ taxi/data/get_random_address.py
@@ -1,38 +1,47 @@
-import requests
-from bs4 import BeautifulSoup
-from random import shuffle
-import clipboard
-from dotenv import load_dotenv
 import os
 from pathlib import Path
+from random import shuffle
+
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Access variables using os.environ
-ADDRESS_BASE_URL = os.environ.get("ADDRESS_BASE_URL")
+ADDRESS_BASE_URL = os.environ.get("ADDRESS_BASE_URL", "")
+DATA_DIR_str = os.environ.get("DATA_DIR", "")
+STORED_ADDRESSES_str = os.environ.get("STORED_ADDRESSES", "")
 
-DATA_DIR = Path(os.environ.get("DATA_DIR"))
-STORED_ADDRESSES = Path(os.environ.get("STORED_ADDRESSES"))
+if "" in (ADDRESS_BASE_URL, DATA_DIR_str, STORED_ADDRESSES_str):
+    raise ValueError(
+        "Environment variables ADDRESS_BASE_URL, DATA_DIR, or STORED_ADDRESSES are not set."
+    )
 
-STORED_ADDRESSES_PATH = DATA_DIR / STORED_ADDRESSES
+DATA_DIR_path = Path(DATA_DIR_str)
+STORED_ADDRESSES_path = Path(STORED_ADDRESSES_str)
+
+STORED_ADDRESSES_PATH = DATA_DIR_path / STORED_ADDRESSES_path
 
 HEADERS = {
-    'user-agent' : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.54",
-    'Accept-Language': 'en,en-GB;q=0.9,en-US;q=0.8,ru;q=0.7',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive',
-    'Referer': 'https://www.google.com/',
-    'DNT': '1',  # Do Not Track Request Header
-    'Upgrade-Insecure-Requests': '1'
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+                   AppleWebKit/537.36 (KHTML, like Gecko) \
+                   Chrome/111.0.0.0 \
+                   Safari/537.36 Edg/111.0.1661.54",
+    "Accept-Language": "en,en-GB;q=0.9,en-US;q=0.8,ru;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Referer": "https://www.google.com/",
+    "DNT": "1",  # Do Not Track Request Header
+    "Upgrade-Insecure-Requests": "1",
 }
-
 
 
 def collect_addresses(base_url: str) -> list[str]:
     """
-    Fetches house addresses from the provided URL, expecting them to be organized in html table.
-
+    Fetches house addresses from the provided URL,
+    expecting them to be organized in html table.
     :param base_url: URL of a webpage containing city addresses.
     :return: list of addresses.
     """
@@ -44,7 +53,9 @@ def collect_addresses(base_url: str) -> list[str]:
 
     while True:
         print(f"Getting {page_number}/ ~ 90")
-        response = session.get(base_url + f"?page={page_number}", cookies={'from-my': 'browser'})
+        response = session.get(
+            base_url + f"?page={page_number}", cookies={"from-my": "browser"}
+        )
         page_number += 1
 
         soup = BeautifulSoup(response.text, "html.parser")
@@ -60,13 +71,15 @@ def collect_addresses(base_url: str) -> list[str]:
 
     return addresses
 
+
 def get_random_address() -> str:
-    '''
-    Tries to read one random address from STORED_ADDRESSES_PATH, else collects addresses using internet.
+    """
+    Tries to read one random address from STORED_ADDRESSES_PATH,
+    else collects addresses using internet.
     :return: one single address presented as str
-    '''
+    """
     try:
-        with open(STORED_ADDRESSES_PATH, 'r') as file:
+        with open(STORED_ADDRESSES_PATH, "r") as file:
             addresses = file.read().splitlines()
             if len(addresses) == 0:
                 raise EOFError
@@ -74,12 +87,13 @@ def get_random_address() -> str:
     except (FileNotFoundError, EOFError):
         print("File not found, collecting data...")
         addresses = collect_addresses(ADDRESS_BASE_URL)
-        with open(STORED_ADDRESSES_PATH, 'w') as file:
+        with open(STORED_ADDRESSES_PATH, "w") as file:
             for address in addresses:
                 file.write(f"{address}\n")
 
     shuffle(addresses)
     return addresses[0]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     get_random_address()
